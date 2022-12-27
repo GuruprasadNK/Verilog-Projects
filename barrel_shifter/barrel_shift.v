b8f444baacb630ca20c30b6db0c22d3dc2367993
GuruprasadNK <121507171+GuruprasadNK@users.noreply.github.com>
2022-12-27 21:53:42 +0530

    Update barrel_shift.v

--- barrel_shifter/barrel_shift.v
+++ barrel_shifter/barrel_shift.v
@@ -1,1 +1,18 @@
 
+module barrel_8_bit(in,n,lr,out);
+  input [7:0]in;
+  input [2:0]n;//number of shift
+  input lr;//left or right shift control sigal
+  output reg[7:0]out;
+  always@(*)
+    begin
+      if(lr)
+        begin
+          out=in<<n;
+        end
+      else
+        begin
+          out=in>>n;
+        end
+    end
+endmodule
