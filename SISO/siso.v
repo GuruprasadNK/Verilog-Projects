6f6673c61f10d789cb444cb0f7d44c3cd2be4583
GuruprasadNK <121507171+GuruprasadNK@users.noreply.github.com>
2022-12-30 19:44:41 +0530

    Add files via upload

--- SISO/siso.v
+++ SISO/siso.v
@@ -1,1 +1,45 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/29/2022 11:49:30 AM
+// Design Name: 
+// Module Name: siso
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
+
+module siso(clk,rst,si,so);
+input clk,rst;
+input [3:0]si;
+output reg [3:0] so;
+reg[3:0]temp;
+
+always@(posedge clk , posedge rst)
+    begin
+        if(rst)
+            temp<=0;
+       
+        else
+            begin
+            
+            temp<=temp<<1;
+            temp[0]<=si;
+            so = temp[3];
+            end
+       end
+ endmodule
+            
+            
+            
